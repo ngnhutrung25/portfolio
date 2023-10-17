8feef69f34f1f57fc412c331cf463394431cfd2e
Michittio <nguyennhutrung.nnt@gmail.com>
2023-10-17 15:18:47 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 // import { useState } from "react";
 // import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
     // const [count, setCount] = useState(0);
 
     return (
         <>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <div className="flex justify-center items-center">
+                <p className="font-light text-lg">
+                    Click on the Vite and React logos to learn more
+                </p>
+            </div>
         </>
     );
 }
